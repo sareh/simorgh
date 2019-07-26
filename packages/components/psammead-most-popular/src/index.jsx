56379fd98c0f792df6a203feff3ab36b370a87ad
Sareh Heidari <sareh.heidari@gmail.com>
2019-07-26 09:07:44 +0100

    Most popular - use input provider for stories data & update styles

--- packages/components/psammead-most-popular/src/index.jsx
+++ packages/components/psammead-most-popular/src/index.jsx
@@ -1,35 +1,64 @@
 import React from 'react';
 import { arrayOf, shape, string } from 'prop-types';
 import styled from 'styled-components';
-import { getSansRegular, getSansBold } from '@bbc/psammead-styles/font-styles';
+import {
+  getSansRegular,
+  getSerifRegular,
+  // getSansBold,
+  // getSerifLight,
+} from '@bbc/psammead-styles/font-styles';
 import { scriptPropType } from '@bbc/gel-foundations/prop-types';
-import { getLongPrimer } from '@bbc/gel-foundations/typography';
+import {
+  getCanon,
+  getGreatPrimer,
+  getTrafalgar,
+  // getRoyal,
+} from '@bbc/gel-foundations/typography';
+import { GEL_SPACING } from '@bbc/gel-foundations/spacings';
+import { C_EBON, C_POSTBOX } from '@bbc/psammead-styles/colours';
 
-const numerals = ['۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۱۰'];
+const EasternArabic = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۱۰'];
 
 const MostPopular = ({ script, service, title, links }) => {
+  console.log('script', script);
+  console.log('service', service);
+
   const StyledHeading = styled.h2`
-    ${script ? getLongPrimer(script) : ''}
-    ${getSansRegular(service)}
+    ${getSansRegular(script)}
+    ${getTrafalgar(script)}
   `;
+  const StyledOl = styled.ol`
+    list-style-type: none;
+  `;
+  const StyledLi = styled.li``;
   const StyledNumeral = styled.span`
-    ${script ? getLongPrimer(script) : ''}
+    color: ${C_POSTBOX};
+    ${getSerifRegular(script)}
+    ${getCanon(script)}
+    padding: ${GEL_SPACING}
   `;
   const StyledLink = styled.a`
-    ${getSansBold(service)}
+    color: ${C_EBON};
+    ${getGreatPrimer(script)}
+    text-decoration: none;
+
+    &:focus,
+    &:hover {
+      text-decoration: underline;
+    }
   `;
 
   return (
     <div role="region">
       <StyledHeading>{title}</StyledHeading>
-      <ol>
-        {links.forEach((link, index) => (
-          <li>
-            <StyledNumeral>{numerals[index]}</StyledNumeral>
+      <StyledOl>
+        {links.map((link, i) => (
+          <StyledLi key={link.text}>
+            <StyledNumeral>{EasternArabic[i + 1]}</StyledNumeral>
             <StyledLink href={link.href}>{link.text}</StyledLink>
-          </li>
+          </StyledLi>
         ))}
-      </ol>
+      </StyledOl>
     </div>
   );
 };
--- packages/components/psammead-most-popular/src/index.stories.jsx
+++ packages/components/psammead-most-popular/src/index.stories.jsx
@@ -1,63 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { arabic } from '@bbc/gel-foundations/scripts';
-import MostPopular from '.';
+import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import notes from '../README.md';
+import MostPopular from '.';
 
-const links = [
-  {
+const links = linkText =>
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => ({
+    text: linkText,
     href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-  {
-    href: 'https://www.bbc.com/persian',
-    text: 'ایستگاه خبر؛ چهارشنبه ۳ بهمن',
-  },
-];
+  }));
 
-/* eslint-disable */
 storiesOf('Components|MostPopular', module)
   .addDecorator(withKnobs)
   .add(
     'default',
-    <MostPopular
-      links={links}
-      script={arabic}
-    service="persian"
-      title="بیشتر بخوانید"
-  />,
+    inputProvider(
+      [{ name: 'Link text' }],
+      ({ slotTexts: [linkText], script, service }) => (
+        <Fragment>
+          <MostPopular
+            links={links(linkText)}
+            script={script || 'arabic'}
+            service={service || 'persian'}
+            title={linkText}
+          />
+          <br />
+          <br />
+          <div lang="en-GB" dir="ltr">
+            Please note this component is currently in an alpha state - it has
+            not had a full accessibility review carried out. It is not ready for
+            use in production environments.
+          </div>
+        </Fragment>
+      ),
+    ),
     { notes, knobs: { escapeHTML: false } },
   );
